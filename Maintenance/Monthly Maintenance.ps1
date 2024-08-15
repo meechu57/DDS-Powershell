54a97b2eb32dc05b17c6aab029feb06815bf0dc8
Meechu57 <BRADWEIAND@gmail.com>
2024-08-15 12:22:08 -0400

    Updated naming scheme in pending reboot & added the script to monthly maintenance.

--- Maintenance/Monthly Maintenance.ps1
+++ Maintenance/Monthly Maintenance.ps1
@@ -7,6 +7,62 @@ switch ($env:scriptRunType) {
   Default { Write-Host "An error occurred when trying to set the log pathway. Setting the log path to the default." ; $logPath = "C:\DDS\Logs\Scripts.log" }
 }
 
+function Enable-PendingReboot {
+    Write-Host "Setting the computer to 'Pending Reboot' state..."
+    Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Setting the computer to 'Pending Reboot' state..."
+
+    # Registry keys and pathway
+    $rebootRequiredRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+    $keysToCheck = @(
+        "c9f3440f-77ba-40bb-a006-1e67387c6e96",
+        "dce6a540-cc87-4c55-8db6-63af55cfd9fd",
+        "7270ec06-f01f-4cda-8db5-a29207141a43"
+    )
+
+    # Check if the RebootRequired key exists
+    if (Test-Path $rebootRequiredRegPath) {
+      # Check for the three keys above
+      foreach ($key in $keysToCheck) {
+        $value = Get-ItemProperty -Path $rebootRequiredRegPath -Name $key -ErrorAction SilentlyContinue
+    
+        # Set the key to the correct value if it doesn't exist or isn't set properly
+        if ($value -ne 1 -or $value -eq $null) {
+          try {
+            Set-ItemProperty -Path $rebootRequiredRegPath -Name $key -Value 1
+          } catch {
+            Write-Host "An error occurred when setting the value of the registry key $key `n $_"
+            Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred when setting the value of the registry key $key `n $_"
+
+            exit 1
+          }
+        }
+      }
+    } # Create the RebootRequired key and subkeys if it doesn't exist 
+    else {
+      # Create the RebootRequired key
+      try {
+        New-Item -Path $rebootRequiredRegPath -Force
+      } catch {
+        Write-Host "An error occurred when trying to create the RebootRequired key: $_"
+        Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred when trying to create the RebootRequired key: $_"
+
+        exit 1
+      }
+  
+      # Create the subkeys
+      foreach ($key in $keysToCheck) {
+        try {
+          New-ItemProperty -Path $rebootRequiredRegPath -Name $key -PropertyType DWord -Value 1 -Force
+        } catch {
+          Write-Host "An error occurred when setting the value of the registry key $key `n $_"
+          Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred when setting the value of the registry key $key `n $_"
+
+          exit 1
+        }
+      }
+    }
+}
+
 # Pull the custom field and trim the end of the audit results.
 $auditInput = Ninja-Property-Get auditResults
 $auditInput = $auditInput.Trim()
@@ -202,6 +258,8 @@ if ($modernStandby.Value -and !$isLaptop) {
       Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Modern Standby is already disabled."
     }
   }
+    # Put the device in a pending reboot state.
+    Enable-PendingReboot
 }
 
 if ($uac.Value) {
@@ -855,6 +913,9 @@ if ($nic.Value) {
     Write-Host "$errors errors occurred while configuring the NIC(s)."
     Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") $errors errors occurred while configuring the NIC(s)."
   }
+
+    # Put device in pending reboot state.
+    Enable-PendingReboot
 }
 
 if ($usb.Value) { 
--- Miscellaneous Scripts/Put Computer in Pending Reboot State.ps1
+++ Miscellaneous Scripts/Put Computer in Pending Reboot State.ps1
@@ -11,7 +11,7 @@ Write-Host "Setting the computer to 'Pending Reboot' state..."
 Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Setting the computer to 'Pending Reboot' state..."
 
 # Registry keys and pathway
-$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+$rebootRequiredRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
 $keysToCheck = @(
     "c9f3440f-77ba-40bb-a006-1e67387c6e96",
     "dce6a540-cc87-4c55-8db6-63af55cfd9fd",
@@ -19,15 +19,15 @@ $keysToCheck = @(
 )
 
 # Check if the RebootRequired key exists
-if (Test-Path $regPath) {
+if (Test-Path $rebootRequiredRegPath) {
   # Check for the three keys above
   foreach ($key in $keysToCheck) {
-    $value = Get-ItemProperty -Path $regPath -Name $key -ErrorAction SilentlyContinue
+    $value = Get-ItemProperty -Path $rebootRequiredRegPath -Name $key -ErrorAction SilentlyContinue
     
     # Set the key to the correct value if it doesn't exist or isn't set properly
     if ($value -ne 1 -or $value -eq $null) {
       try {
-        Set-ItemProperty -Path $regPath -Name $key -Value 1
+        Set-ItemProperty -Path $rebootRequiredRegPath -Name $key -Value 1
       } catch {
         Write-Host "An error occurred when setting the value of the registry key $key `n $_"
         Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred when setting the value of the registry key $key `n $_"
@@ -40,7 +40,7 @@ if (Test-Path $regPath) {
 else {
   # Create the RebootRequired key
   try {
-    New-Item -Path $regPath -Force
+    New-Item -Path $rebootRequiredRegPath -Force
   } catch {
     Write-Host "An error occurred when trying to create the RebootRequired key: $_"
     Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred when trying to create the RebootRequired key: $_"
@@ -51,7 +51,7 @@ else {
   # Create the subkeys
   foreach ($key in $keysToCheck) {
     try {
-      New-ItemProperty -Path $regPath -Name $key -PropertyType DWord -Value 1 -Force
+      New-ItemProperty -Path $rebootRequiredRegPath -Name $key -PropertyType DWord -Value 1 -Force
     } catch {
       Write-Host "An error occurred when setting the value of the registry key $key `n $_"
       Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred when setting the value of the registry key $key `n $_"
