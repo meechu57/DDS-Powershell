ada45337e881f839cd4a9d20c124c070a8897f44
Meechu57 <BRADWEIAND@gmail.com>
2024-09-03 15:34:16 -0400

    Added a setting to change the current BIOS password and reboot the computer.

--- Staging Scripts/Configure Dell BIOS.ps1
+++ Staging Scripts/Configure Dell BIOS.ps1
@@ -13,8 +13,10 @@ $uninstallKeys = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*', 
 # To track if the redistributable is installed.
 $isInstalled = $false
 
-# For error tracking.
-$errors = 0
+# Pull the script variables.
+$reboot = $env:rebootComputer
+$newPW = $env:newBiosPassword
+$currentPW = $env:currentBiosPassword
 
 # Go through both registry locations.
 foreach ($uKey in $uninstallKeys) {
@@ -25,6 +27,9 @@ foreach ($uKey in $uninstallKeys) {
       Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") C++ 2015-2022 Redistributable is already installed."
       
       $isInstalled = $true
+    }
+    if ($isInstalled) {
+        break
     }
   }
 }
@@ -38,6 +43,8 @@ if ($isInstalled -eq $false) {
     } catch {
       Write-Host "An error occurred creating the temp directory: $_"
       Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred creating the temp directory: $_"
+
+      exit 1
     }
   } 
   
@@ -51,7 +58,7 @@ if ($isInstalled -eq $false) {
   Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Installing vc_redist.x64.exe..."
   
   # Installs the redistributable.
-  Start-Process -FilePath "C:\DDS\Temp\vc_redist.x64.exe" -ArgumentList "/q"
+  Start-Process -FilePath "C:\DDS\Temp\vc_redist.x64.exe" -ArgumentList "/q /norestart"
 }
 
 # Pulls the list of installed packages, selecting the names.
@@ -67,8 +74,13 @@ if (!($packages -contains "NuGet")) {
   } catch {
     Write-Host "An error occured while installing NuGet: $_"
     Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occured while installing NuGet: $_"
+
+    exit 1
   }
 }
+
+# Yoinked from another script. Unknown if this is needed as of now.
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
 
 # Pulls the list of installed PowerShell modules, selecting the names.
 $modules = Get-Module -ListAvailable | Select-Object -ExpandProperty Name
@@ -85,68 +97,166 @@ if (!($modules -contains "DellBIOSProvider")) {
   } catch {
     Write-Host "An error occured while installing DellBIOSProvider: $_"
     Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occured while installing DellBIOSProvider: $_"
+
+    exit 1
   }
+} else {
+    Import-Module -Name DellBIOSProvider
 }
+
+# Set the execution policy for this script.
+Set-ExecutionPolicy -ExecutionPolicy remotesigned -Scope Process
+
 
 Write-Host "Configuring BIOS settings..."
 Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Configuring BIOS settings..."
 
-# Start configuring BIOS settings.
-try {
-  Set-Item -Path DellSmbios:\PowerManagement\AcPwrRcvry "Last"
-} catch {
-  Write-Host "An error occurred while setting AC Power Recovery: $_"
-  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting AC Power Recovery: $_"
-  
-  $errors++
-}
+# For error tracking.
+$errors = 0
 
-try {
-  Set-Item -Path DellSmbios:\PowerManagement\DeepSleepCtrl "Disabled"
-} catch {
-  Write-Host "An error occurred while setting Deel Sleep Control: $_"
-  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Deel Sleep Control: $_"
-  
-  $errors++
-}
+# Check if a BIOS pw is already set.
+$pwSet = (Get-Item -Path DellSmbios:\Security\IsAdminPasswordSet).CurrentValue
 
-try {
-  Set-Item -Path DellSmbios:\PowerManagement\WakeOnLan "LanOnly"
-}catch {
-  Write-Host "An error occurred while setting Wake On Lan: $_"
-  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Wake On Lan: $_"
-  
-  $errors++
-}
+if ($pwSet -eq "True") {
+  if ($currentPW -eq $null) {
+    Write-Host "There is currently a BIOS password set. Please input the password in the 'Current BIOS Password' script variable."
+    Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") There is currently a BIOS password set. Please input the password in the 'Current BIOS Password' script variable."
 
-try {
-  Set-Item -Path DellSmbios:\Performance\CStatesCtrl "Disabled"
-}catch {
-  Write-Host "An error occurred while setting C States Control: $_"
-  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting C States Control: $_"
-  
-  $errors++
-}
+    exit 1
+  }
 
-try {
-  Set-Item -Path DellSmbios:\PowerManagement\BlockSleep "Enabled"
-}catch {
-  Write-Host "An error occurred while setting Block Sleep: $_"
-  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Block Sleep: $_"
-  
-  $errors++
-}
-
-if ($env:setBiosPassword -eq $true) {
+  # Start configuring BIOS settings.
   try {
-    Set-Item -Path DellSmbios:\Security\AdminPassword $env:biosPassword
+    Set-Item -Path DellSmbios:\PowerManagement\WakeOnLan "LanOnly" -Password $currentPW
   } catch {
-    Write-Host "An error occurred while setting the BIOS password: $_"
-    Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting the BIOS password: $_"
-    
+    Write-Host "An error occurred while setting Wake On Lan: $_"
+    Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Wake On Lan: $_"
+
     $errors++
   }
+
+  # Check if running on a laptop or desktop
+  $battery = Get-CimInstance Win32_Battery
+  if ($battery -eq $null -or $battery.name -like "*UPS*") {
+   try {
+      Set-Item -Path DellSmbios:\PowerManagement\BlockSleep "Enabled" -Password $currentPW
+    } catch {
+      Write-Host "An error occurred while setting Block Sleep: $_"
+      Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Block Sleep: $_"
+  
+      $errors++
+    }
+    try {
+      Set-Item -Path DellSmbios:\PowerManagement\AcPwrRcvry "Last" -Password $currentPW
+    } catch {
+      Write-Host "An error occurred while setting AC Power Recovery: $_"
+      Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting AC Power Recovery: $_"
+
+      $errors++
+    }
+
+    try {
+      Set-Item -Path DellSmbios:\PowerManagement\DeepSleepCtrl "Disabled" -Password $currentPW
+    } catch {
+      Write-Host "An error occurred while setting Deep Sleep Control: $_"
+      Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Deep Sleep Control: $_"
+
+      $errors++
+    }
+  }
+
+  try {
+    Set-Item -Path DellSmbios:\Performance\CStatesCtrl "Disabled" -Password $currentPW
+  }catch {
+    Write-Host "An error occurred while setting C States Control: $_"
+    Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting C States Control: $_"
+
+    $errors++
+  }
+
+  if ($newPW) {
+    try {
+      Set-Item -Path DellSmbios:\Security\AdminPassword $newPW -Password $currentPW
+    } catch {
+      Write-Host "An error occurred while setting the BIOS password: $_"
+      Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting the BIOS password: $_"
+  
+      $errors++
+    }
+  }
+} elseif ($pwSet -eq "False") {
+    # Start configuring BIOS settings.
+    try {
+      Set-Item -Path DellSmbios:\PowerManagement\WakeOnLan "LanOnly"
+    }catch {
+      Write-Host "An error occurred while setting Wake On Lan: $_"
+      Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Wake On Lan: $_"
+  
+      $errors++
+    }
+
+    # Check if running on a laptop or desktop
+    $battery = Get-CimInstance Win32_Battery
+    if ($battery -eq $null -or $battery.name -like "*UPS*") {
+     try {
+        Set-Item -Path DellSmbios:\PowerManagement\BlockSleep "Enabled"
+      }catch {
+        Write-Host "An error occurred while setting Block Sleep: $_"
+        Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Block Sleep: $_"
+    
+        $errors++
+      }
+      try {
+        Set-Item -Path DellSmbios:\PowerManagement\AcPwrRcvry "Last"
+      } catch {
+        Write-Host "An error occurred while setting AC Power Recovery: $_"
+        Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting AC Power Recovery: $_"
+
+        $errors++
+      }
+
+      try {
+        Set-Item -Path DellSmbios:\PowerManagement\DeepSleepCtrl "Disabled"
+      } catch {
+        Write-Host "An error occurred while setting Deep Sleep Control: $_"
+        Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting Deep Sleep Control: $_"
+
+        $errors++
+      }
+    }
+
+    try {
+      Set-Item -Path DellSmbios:\Performance\CStatesCtrl "Disabled"
+    }catch {
+      Write-Host "An error occurred while setting C States Control: $_"
+      Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting C States Control: $_"
+  
+      $errors++
+    }
+
+    if ($env:newBiosPassword -eq $true) {
+      try {
+        Set-Item -Path DellSmbios:\Security\AdminPassword $env:newBiosPassword
+      } catch {
+        Write-Host "An error occurred while setting the BIOS password: $_"
+        Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") An error occurred while setting the BIOS password: $_"
+    
+        $errors++
+      }
+    }
 }
 
-Write-Host "Finished configuring BIOS settings with $errors errors."
-Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Finished configuring BIOS settings with $errors errors."
+if ($errors -and $errors -ne 0) {
+  Write-Host "Finished configuring BIOS settings with $errors errors."
+  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Finished configuring BIOS settings with $errors errors."
+} else {
+  Write-Host "Finished configuring BIOS settings with 0 errors."
+  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Finished configuring BIOS settings with 0 errors."
+}
+
+if ($reboot -and $reboot -eq $true) {
+  Write-Host "Reboot option selected. Rebooting the comtpuer..."
+  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Reboot option selected. Rebooting the comtpuer..."
+  
+  shutdown /r
+}
