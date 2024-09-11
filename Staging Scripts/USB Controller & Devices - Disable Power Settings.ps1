26c1b9390ec328d8679afd97499a2e952238c68d
Meechu57 <BRADWEIAND@gmail.com>
2024-09-11 12:35:43 -0400

    Updated Exit codes.

--- Staging Scripts/USB Controller & Devices - Disable Power Settings.ps1
+++ Staging Scripts/USB Controller & Devices - Disable Power Settings.ps1
@@ -10,30 +10,22 @@ switch ($env:scriptRunType) {
 # For error tracking.
 $errors = 0
 
-Write-Host "Configuring power settings for all USB devices and controllers."
-Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Configuring power settings for all USB devices and controllers."
-
-try {
-  # Set the power option for USB controllers
-  Set-CimInstance -Query 'SELECT * FROM MSPower_DeviceEnable WHERE InstanceName LIKE "USB\\%"' -Namespace root/WMI -Property @{Enable = $false}
-} catch {
-  Write-Host "Failed to disable 'Allow the computer to turn off this device to save power' on all USB controllers: $_"
-  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Failed to disable 'Allow the computer to turn off this device to save power' on all USB controllers.: $_"
-  
-  $errors++
-}
+Write-Host "Configuring power settings for all USB devices and controllers..."
+Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Configuring power settings for all USB devices and controllers..."
 
 try {
   # Dynamic power devices
   $powerMgmt = Get-CimInstance -ClassName MSPower_DeviceEnable -Namespace root/WMI
 
-  # All USB devices
-  $usbDevices = Get-CimInstance -ClassName Win32_PnPEntity -Filter 'PNPClass = "USB"'
-
-  $usbDevices | ForEach-Object {
-  # Get the power management instance for this device, if there is one.
-  $powerMgmt | Where-Object InstanceName -Like "*$($_.PNPDeviceID)*"
-  } | Set-CimInstance -Property @{Enable = $false}
+  # Get all USB devices with dynamic power options
+  $usbDevices = Get-CimInstance -ClassName Win32_PnPEntity |
+      Select-Object Name, @{ Name = "Enable"; Expression = { 
+          $powerMgmt | Where-Object InstanceName -Like "*$($_.PNPDeviceID)*" | Select-Object -ExpandProperty Enable }} |
+              Where-Object { $null -ne $_.Enable -and $_.Enable -eq $true } |  
+                  Where-Object {$_.Name -like "*USB*" -and $_.Name -notlike "*Virtual*"}
+  
+  # Try to disable the power option on each USB devcie from above
+  $powerMgmt | Where-Object { $_.InstanceName -Like "*$($usbDevice.PNPDeviceID)*" } | Set-CimInstance -Property @{Enable = $false}
 } catch {
   Write-Host "Failed to disable 'Allow the computer to turn off this device to save power' on all USB devices: $_"
   Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Failed to disable 'Allow the computer to turn off this device to save power' on all USB devices: $_"
@@ -41,9 +33,13 @@ try {
   $errors++
 }
 
-Write-Host "Finished configuring power settings for all USB devices and controllers with $errors errors."
-Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Finished configuring power settings for all USB devices and controllers with $errors errors."
-
-
-
-# Credit: https://www.reddit.com/r/PowerShell/comments/lr5iyk/comment/goladae/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+# Show any errors.
+if ($errors -and $errors -eq 0) {
+  Write-Host "Finished configuring power settings for all USB devices and controllers with 0 errors."
+  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Finished configuring power settings for all USB devices and controllers with 0 errors."
+} else {
+  Write-Host "Finished configuring power settings for all USB devices and controllers with $errors errors."
+  Add-Content -Path $logPath -Value "$(Get-Date -UFormat "%Y/%m/%d %T:") Finished configuring power settings for all USB devices and controllers with $errors errors."
+  
+  exit 1
+}
